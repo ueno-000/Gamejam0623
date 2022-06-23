b0af8195ef343729343c0aa9b6c3d14de1a4f5ee
SR21C <rysawad@gmail.com>
2022-06-23 12:15:34 +0900

    [Add] NormalBlockのPrefabを追加

--- Assets/Sawada/Scripts/BlockBase.cs
+++ Assets/Sawada/Scripts/BlockBase.cs
@@ -6,15 +6,18 @@ public class BlockBase : MonoBehaviour
 {
     [SerializeField] float _breakVerocity = 0.1f;
     Rigidbody2D _rb;
+    Vector3 _beforePos; 
     void OnEnable()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _beforePos = transform.position;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(_rb.velocity.y >= _breakVerocity)
+        if(_rb.velocity.y >= _breakVerocity && 
+            collision.gameObject.TryGetComponent<BlockBase>(out BlockBase b))
         {
-            Debug.Log("Break");
+            Debug.Log($"Break:{gameObject.name}");
         }
     }
     void BreakBlock()
