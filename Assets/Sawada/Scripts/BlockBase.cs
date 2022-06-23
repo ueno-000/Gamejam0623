2c64cb555513628417bd883a8cb2f4ac49553972
SR21C <rysawad@gmail.com>
2022-06-23 14:09:32 +0900

    [Add] UnbreakableBlock(IronBrock)を追加 [Update] BrockBase完成 [Derete] BlockColorを削除

--- Assets/Sawada/Scripts/BlockBase.cs
+++ Assets/Sawada/Scripts/BlockBase.cs
@@ -4,24 +4,42 @@ using UnityEngine;
 
 public class BlockBase : MonoBehaviour
 {
-    [SerializeField] float _breakVerocity = 0.1f;
+    [SerializeField] bool _isBreakable = false;
+    [SerializeField] float _breakVerocity = 10f;
+    //[SerializeField] ScoreManager scoreManager;
+    bool _isActive = true;
     Rigidbody2D _rb;
-    Vector3 _beforePos; 
+    
     void OnEnable()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _beforePos = transform.position;
+        //scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    void Update()
     {
-        if(_rb.velocity.y >= _breakVerocity && 
-            collision.gameObject.TryGetComponent<BlockBase>(out BlockBase b))
+        if(_rb.velocity.y < -_breakVerocity)
         {
-            Debug.Log($"Break:{gameObject.name}");
+            Debug.Log(Mathf.Abs(_rb.velocity.y));
+            _isActive = false;
+            _isBreakable = true;
         }
     }
-    void BreakBlock()
-    {
 
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(!_isActive)
+        {
+            BreakBrock();
+        }
+    }
+    void BreakBrock()
+    {
+        if (!_isBreakable)return;
+        else
+        {
+            //scoreManager.ScoreUp();
+            Destroy(gameObject);
+        }
     }
 }
